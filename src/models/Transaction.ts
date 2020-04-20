464899ada6bef460265ff8b3098504af41dddc8f
Gabriel <gabrieldiasbnu@hotmail.com>
2020-04-20 23:33:39 +0200

    First commit

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  title: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 
-  type: 'income' | 'outcome';
-
-  value: number;
-
+  @Column()
   category_id: string;
 
+  @Column()
+  title: string;
+
+  @Column()
+  type: 'income' | 'outcome';
+
+  @Column('decimal')
+  value: number;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
